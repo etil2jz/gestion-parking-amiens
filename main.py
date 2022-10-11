8e7a541fbc5e0d944d3d7f52cdf647daac08b54e
etil2jz <81570777+etil2jz@users.noreply.github.com>
2022-10-11 18:24:29 +0200

    oui

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+import csv
+
+# La structure parks est remplie des données de parking du fichier parking-metropole.txt
+# Cela donne concrètement un tableau à 2 dimensions (parks[0][0] retourne 'Identifiant')
+parks = []
+
+def chargerFichierParking():
+    with open("parking-metropole.txt", "r") as fichierParking:
+        lecture = csv.reader(fichierParking, delimiter = '	')
+        next(lecture)
+        for ligne in lecture:
+            parks.append(ligne)
+    print(parks)
+
+def main():
+    print("\n=============================================\nBienvenue sur Amiens Métropole CarPark Valpha\n=============================================\n")
+    chargerFichierParking()
+
+main()
